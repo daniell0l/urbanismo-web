102d6501cb4a91e608b453d14581bbbd73cd4a4a
daniell0l <daniel_l0l@hotmail.com>
2023-07-24 17:32:00 -0300

    feat: add icon in info item

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,6 +1,11 @@
 import "./style.css";
-import { FaUser } from "react-icons/fa";
-import { FaSearch } from "react-icons/fa";
+import {
+  FaUser,
+  FaSearch,
+  FaUserPlus,
+  FaDownload,
+  FaInfoCircle,
+} from "react-icons/fa";
 import logoWide from "../../assets/imgs/logoWide.png";
 import logoModel from "../../assets/imgs/logoModel.png";
 
@@ -43,59 +48,101 @@ export function Home() {
       </header>
 
       <main>
-        <section className="paragrafo">
-          <h1>LEM URBANISMO</h1>
-          <div className="linha"></div>
-          <p>
-            Sistema de Planejamento, Orçamento e Gestão tem a finalidade de
-            virtualizar todo o processo e etapas de elaboração de processos de
-            Alvará de Construção, Regularização, Acréscimo de Área,
-            Desmembramento, Remembramento e Redimensionamento.
-          </p>
-        </section>
-        <section className="modelo_png">
-          <div className="bloco">
-            <div className="circulo">
-            <img src={logoModel} alt="mulher modelo" />
+        <div className="conteudo">
+          <section className="paragrafo">
+            <h1>
+              LEM <br /> URBANISMO
+            </h1>
+            <div className="linha"></div>
+            <p>
+              Sistema de Planejamento, Orçamento e Gestão tem a finalidade de
+              virtualizar todo o processo e etapas de elaboração de processos de
+              Alvará de Construção, Regularização, Acréscimo de Área,
+              Desmembramento, Remembramento e Redimensionamento.
+            </p>
+          </section>
+          <section className="modelo_png">
+            <div className="bloco">
+              <div className="circulo">
+                <img src={logoModel} alt="mulher modelo" />
+              </div>
+            </div>
+          </section>
+        </div>
+        <section id="conteudo_final">
+          <div className="info_down_cad">
+            <div className="info_item">
+              <a href="/">
+                <FaInfoCircle color="3a28a"/>
+              </a>
+              <div className="info_text">
+                <h2>informativo</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                  diam nonummy nibh euismod tincidunt ut laoreet dolorediam
+                  nonummy nibh euismod tincidunt ut laoreet dolorediam nonummy
+                  nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+                  volutpat.
+                </p>
+              </div>
+            </div>
+
+            <div className="info_item">
+              <a href="/">
+                <FaDownload color="3a28a"/>
+              </a>
+              <div className="info_text">
+                <h2>download</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                  diam nonummy nibh euismod tincidunt ut laoreet dolorediam
+                  nonummy nibh euismod tincidunt ut laoreet dolorediam nonummy
+                  nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+                  volutpat.
+                </p>
+              </div>
+            </div>
+
+            <div className="info_item">
+              <a href="/">
+                <FaUserPlus color="3a28a"/>
+              </a>
+              <div className="info_text">
+                <h2>cadastro</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                  diam nonummy nibh euismod tincidunt ut laoreet dolorediam
+                  nonummy nibh euismod tincidunt ut laoreet dolorediam nonummy
+                  nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+                  volutpat.
+                </p>
+              </div>
             </div>
           </div>
         </section>
-        {/* <section id="redes_sociais">
-          <button
-            style={{
-              background: "transparent",
-              border: "none",
-              cursor: "pointer",
-            }}
-          ></button>
-          <button
-            style={{
-              background: "transparent",
-              border: "none",
-              cursor: "pointer",
-            }}
-          ></button>
-        </section> */}
-      </main>
 
-      <section id="conteudo_final">
-        <div className="info_down_cad">
-          <a href="/">informativo</a>
-          <a href="/">download</a>
-          <a href="/">cadastro</a>
-        </div>
-        <div className="area_prof_tra">
-          <a href="/">área profissional</a>
-          <a href="/">área trabalho</a>
-        </div>
-      </section>
+        <section>
+          <div className="area_prof_tra">
+            <div className="statistica"></div>
+            <div className="botao-container">
+              <a href="/" id="profissional-area">
+                <h2>Área Profissional</h2>
+              </a>
+              <a href="/" id="work-area">
+                <h2>Área Trabalho</h2>
+              </a>
+            </div>
+          </div>
+        </section>
+      </main>
 
       <footer id="rodape">
         <div>
           <img src="assets/imgs/logo.png" alt="Logo" />
           <p>
-            Prefeitura Municipal de Luís Eduardo Magalhães Telefone: 77
-            3628-9000 - Luís Eduardo Magalhães - Bahia
+            Prefeitura Municipal de Luís Eduardo Magalhães <br />
+            CEP: 47.850-000 - Luís Eduardo Magalhães - Bahia <br />
+            Telefone: 77 3628-9000
           </p>
           <p>
             © {new Date().getFullYear()} VALLE TEC LAB todos os direitos
