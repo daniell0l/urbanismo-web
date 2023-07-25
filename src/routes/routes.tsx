f9706698307535d03d7a99a0607b6d67a9a0fbda
pablo <wademcfild@gmail.com>
2023-07-25 14:10:37 -0300

    fazendo o import da tela de login

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from '../screens/home';
+import Login from '../screens/home/login/login'
 
 const AppRoutes: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
       </Routes>
     </Router>
   );
