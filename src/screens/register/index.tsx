3bedf202f5ba96108a49ec12ec6fd7304e797885
pablo <wademcfild@gmail.com>
2023-07-26 16:44:34 -0300

    html da página de registro finalizado

---
+++ src/screens/register/index.tsx
@@ -0,0 +1,87 @@
+import "./Registration.css";
+import React, { useState } from "react";
+
+const RegistrationPage: React.FC = () => {
+  const [name, setName] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const handleCpfChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCpf(event.target.value);
+  };
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleRegistration = () => {
+    console.log("Name:", name);
+    console.log("CPF:", cpf);
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  return (
+    <div className="registration-container">
+      <h2>Registration</h2>
+      <form>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={handleNameChange}
+            placeholder="Enter your name"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="cpf">CPF:</label>
+          <input
+            type="text"
+            id="cpf"
+            value={cpf}
+            onChange={handleCpfChange}
+            placeholder="Enter your CPF"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="Enter your email"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            placeholder="Enter your password"
+          />
+        </div>
+        <button type="button" onClick={handleRegistration}>
+          Register
+        </button>
+        <br />
+        <a href="">Back to login page</a>
+      </form>
+    </div>
+  );
+};
+
+export default RegistrationPage;
