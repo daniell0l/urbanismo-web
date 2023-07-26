57a5bbbbbf7688a07fc664b26fc1b8e6cde7b879
pablo <wademcfild@gmail.com>
2023-07-26 17:50:57 -0300

    estilização da página de cadastro 1

--- src/screens/register/index.tsx
+++ src/screens/register/index.tsx
@@ -1,4 +1,6 @@
+import "./style.css";
 import React, { useState } from "react";
+import logoLateral from "../../assets/imgs/logoWide.png";
 
 const RegistrationPage: React.FC = () => {
   const [name, setName] = useState("");
@@ -30,54 +32,64 @@ const RegistrationPage: React.FC = () => {
   };
 
   return (
-    <div className="registration-container">
-      <h2>Registration</h2>
-      <form>
+    <div id="registration-container">
+      <div className="logoLateral">
+        <img src={logoLateral} alt="logo lem urbanismo" />
+      </div>
+      <div className="linhaVerde"></div>
+      <form className="blocoRegistration">
+        <h2>Cadastro</h2>
         <div className="form-group">
-          <label htmlFor="name">Name:</label>
+          <label htmlFor="name"></label>
           <input
             type="text"
             id="name"
             value={name}
             onChange={handleNameChange}
-            placeholder="Enter your name"
+            placeholder="Usuário"
           />
         </div>
         <div className="form-group">
-          <label htmlFor="cpf">CPF:</label>
-          <input
-            type="text"
-            id="cpf"
-            value={cpf}
-            onChange={handleCpfChange}
-            placeholder="Enter your CPF"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
+          <label htmlFor="email"></label>
           <input
             type="email"
             id="email"
             value={email}
             onChange={handleEmailChange}
-            placeholder="Enter your email"
+            placeholder="Email"
+          />
+          <label htmlFor="cpf"></label>
+          <input
+            type="text"
+            id="cpf"
+            value={cpf}
+            onChange={handleCpfChange}
+            placeholder="CPF"
           />
         </div>
         <div className="form-group">
-          <label htmlFor="password">Password:</label>
+          <label htmlFor="password"></label>
           <input
             type="password"
             id="password"
             value={password}
             onChange={handlePasswordChange}
-            placeholder="Enter your password"
+            placeholder="Senha"
           />
         </div>
-        <button type="button" onClick={handleRegistration}>
-          Register
+        <button
+          className="buttonRegistration"
+          type="button"
+          onClick={handleRegistration}
+        >
+          próximo
         </button>
         <br />
-        <a href="">Back to login page</a>
+        <div  className="cadastroLink">
+          <a href="javascript:history.go(-1)">
+            Voltar a página anterior
+          </a>
+        </div>
       </form>
     </div>
   );
