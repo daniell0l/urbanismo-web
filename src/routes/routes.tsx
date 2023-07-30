11e40c45b76ea363e9a06c2ea39ea5fc132da6f3
Sr.Carrilho <126430073+WadeMcfild@users.noreply.github.com>
2023-07-30 12:07:34 -0300

    criando rota da principal

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from '../screens/home';
 import Login from '../screens/login'
 import Register from '../screens/register/index'
+import Principal from '../screens/principal/index'
 
 const AppRoutes: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const AppRoutes: React.FC = () => {
         <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Register" element={<Register />} />
+        <Route path="/Principal" element={<Principal />} />
       </Routes>
     </Router>
   );
