c04eae46ee40190ece8767db9703c3416f7b7372
Sr.Carrilho <126430073+WadeMcfild@users.noreply.github.com>
2023-07-30 14:25:29 -0300

    criação da index da página principal

--- src/screens/principal/index.tsx
+++ src/screens/principal/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "./style.css";
 
 interface Result {
   id: number;
@@ -10,98 +11,127 @@ interface Result {
 }
 
 const optionsConstruction = [
-  'Construction Permit',
-  'Regularization Permit',
-  'Condominium',
-  'Resizing',
-  'Project Permit',
-  'Subdivision',
+  "Alvará de construção",
+  "Alvará de regularização",
+  "Condomínio",
+  "Redimensionando",
+  "Alvará de projeto",
+  "Subdivisão",
 ];
 
-const optionsProtocol = ['Protocol/Address', 'Permit', 'Occupancy Permit', 'Owners'];
+const optionsProtocol = [
+  "Protocolo/Endereço",
+  "Alvará",
+  "alvará de construção",
+  "Proprietários",
+];
 
 const TableComponent: React.FC = () => {
-  const [selectedConstruction, setSelectedConstruction] = useState<string>('');
-  const [selectedProtocol, setSelectedProtocol] = useState<string>('');
-  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [formState, setFormState] = useState<{
+    selectedConstruction: string;
+    selectedProtocol: string;
+    searchTerm: string;
+  }>({
+    selectedConstruction: "",
+    selectedProtocol: "",
+    searchTerm: "",
+  });
 
-  const [results, setResults] = useState<Result[]>([
+  const [results, setResults] = useState<Result[]>([]);
 
-  ]);
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   const handleSearch = () => {
     const filteredResults = results.filter(
       (result) =>
-        result.ir.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        result.proprietors.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        result.address.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        result.district.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        result.data.toLowerCase().includes(searchTerm.toLowerCase())
+        result.ir.toLowerCase().includes(formState.searchTerm.toLowerCase()) ||
+        result.proprietors.toLowerCase().includes(formState.searchTerm.toLowerCase()) ||
+        result.address.toLowerCase().includes(formState.searchTerm.toLowerCase()) ||
+        result.district.toLowerCase().includes(formState.searchTerm.toLowerCase()) ||
+        result.data.toLowerCase().includes(formState.searchTerm.toLowerCase())
     );
 
     setResults(filteredResults);
   };
 
   return (
-    <div>
-      <div>
-        <select
-          value={selectedConstruction}
-          onChange={(e) => setSelectedConstruction(e.target.value)}
-        >
-          <option value="">Select Construction Type</option>
-          {optionsConstruction.map((option, index) => (
-            <option key={index} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
-
-        <select value={selectedProtocol} onChange={(e) => setSelectedProtocol(e.target.value)}>
-          <option value="">Select Protocol Type</option>
-          {optionsProtocol.map((option, index) => (
-            <option key={index} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
-
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Search..."
-        />
-
-        <button onClick={handleSearch}>Search</button>
+    <div id="principal">
+      <div className="navbar">
+        <div className="construction-protocol">
+          <div className="selectedConstruction">
+            <select
+              name="selectedConstruction"
+              value={formState.selectedConstruction}
+              onChange={handleChange}
+            >
+              <option value="">tipo de projeto</option>
+              {optionsConstruction.map((option, index) => (
+                <option key={index} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="selectedProtocol">
+            <select
+              name="selectedProtocol"
+              value={formState.selectedProtocol}
+              onChange={handleChange}
+            >
+              <option value="">Processos</option>
+              {optionsProtocol.map((option, index) => (
+                <option key={index} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="Search">
+            <input
+              type="text"
+              name="searchTerm"
+              value={formState.searchTerm}
+              onChange={handleChange}
+              placeholder="Pesquisar..."
+            />
+            <button onClick={handleSearch}>Procurar</button>
+          </div>
+        </div>
       </div>
-
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>IR</th>
-            <th>Proprietors</th>
-            <th>Address</th>
-            <th>District</th>
-            <th>Data</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {results.map((result) => (
-            <tr key={result.id}>
-              <td>{result.id}</td>
-              <td>{result.ir}</td>
-              <td>{result.proprietors}</td>
-              <td>{result.address}</td>
-              <td>{result.district}</td>
-              <td>{result.data}</td>
-              <td>Actions</td>
+      <div className="Search-result">
+        <table>
+          <thead>
+            <tr>
+              <th>IR</th>
+              <th>Proprietors</th>
+              <th>Address</th>
+              <th>District</th>
+              <th>Data</th>
+              <th>Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {results.map((result) => (
+              <tr key={result.id}>
+                <td>{result.id}</td>
+                <td>{result.ir}</td>
+                <td>{result.proprietors}</td>
+                <td>{result.address}</td>
+                <td>{result.district}</td>
+                <td>{result.data}</td>
+                <td>Actions</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <a href="javascript:history.go(-1)">voltar a página anterior</a>
     </div>
   );
 };
