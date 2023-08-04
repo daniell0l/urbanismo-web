6d8ee909520944013e5eed5262cecc265fc3bf11
pablo <wademcfild@gmail.com>
2023-08-04 15:10:24 -0300

    modificações no componente da tela do condomínio

--- src/components/AddUnity/index.tsx
+++ src/components/AddUnity/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./style.css"
 
 interface PavementData {
   areaConstruir: string;
@@ -42,11 +43,6 @@ const AddUnitButton: React.FC = () => {
     }
   };
 
-  const handleAddPavements = (index: number) => {
-    setModalUnitIndex(index);
-    setIsModalOpen(true);
-  };
-
   const handleAddPavement = (index: number) => {
     setUnitData((prevData) => {
       const newData = [...prevData];
@@ -72,107 +68,46 @@ const AddUnitButton: React.FC = () => {
     });
   };
 
-  const handleChangePavement = (
-    unitIndex: number,
-    pavementIndex: number,
-    field: keyof PavementData,
-    value: string
-  ) => {
-    setUnitData((prevData) => {
-      const newData = [...prevData];
-      newData[unitIndex].pavements[pavementIndex][field] = value;
-      return newData;
-    });
-  };
-
   return (
     <div>
-      <button onClick={handleAddUnitClick}>Adicionar Unidade</button>
+      <button onClick={handleAddUnitClick}>+</button>
       {unitCount > 0 && (
-        <button onClick={handleRemoveUnitClick}>Remover Unidade</button>
+        <button onClick={handleRemoveUnitClick}>-</button>
       )}
       {Array.from({ length: unitCount }).map((_, index) => (
         <div key={index}>
-          <div>Casa {index + 1}</div>
           <div>
-            <label>Tipo de Processo:</label>
-            <select>
-              <option value="">Selecione</option>
-              <option value="Construção">Construção</option>
-              <option value="Regularização">Regularização</option>
-              <option value="Acréscimo de Área">Acréscimo de Área</option>
-            </select>
-          </div>
-          <div>
-            <label>Loteamento:</label>
             <input
               type="text"
+              placeholder="Loteamento"
               value={unitData[index]?.loteamento}
               onChange={(e) =>
                 handleChange(index, "loteamento", e.target.value)
               }
             />
           </div>
-          <div>
-            <label>Quadra:</label>
+          <div className="minAddres">
             <input
               type="text"
+              placeholder="Quadra"
               value={unitData[index]?.quadra}
               onChange={(e) => handleChange(index, "quadra", e.target.value)}
             />
           </div>
-          <div>
-            <label>Lote:</label>
+          <div className="minAddres">
             <input
               type="text"
+              placeholder="Lote"
               value={unitData[index]?.lote}
               onChange={(e) => handleChange(index, "lote", e.target.value)}
             />
           </div>
-          <div>
-            <label>Nº:</label>
+          <div className="minAddres">
             <input
               type="text"
+              placeholder="Nº"
               value={unitData[index]?.numero}
               onChange={(e) => handleChange(index, "numero", e.target.value)}
-            />
-          </div>
-          <div>
-            <label>Área a Construir(m²):</label>
-            <input
-              type="text"
-              value={unitData[index]?.areaConstruir}
-              onChange={(e) =>
-                handleChange(index, "areaConstruir", e.target.value)
-              }
-            />
-          </div>
-          <div>
-            <label>Área Existente(m²):</label>
-            <input
-              type="text"
-              value={unitData[index]?.areaExistente}
-              onChange={(e) =>
-                handleChange(index, "areaExistente", e.target.value)
-              }
-            />
-          </div>
-          <div>
-            <label>Área de Uso Exclusivo(m²):</label>
-            <input
-              type="text"
-              value={unitData[index]?.areaExclusiva}
-              onChange={(e) =>
-                handleChange(index, "areaExclusiva", e.target.value)
-              }
-            />
-          </div>
-          <div>
-            <label>Área Comum Proporcional(m²):</label>
-            <input
-              type="text"
-              value={unitData[index]?.areaComum}
-              onChange={(e) => handleChange(index, "areaComum", e.target.value)}
             />
           </div>
           <div>
@@ -188,6 +123,44 @@ const AddUnitButton: React.FC = () => {
               <option value="Acréscimo de Área">Acréscimo de Área</option>
             </select>
           </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Área a Construir(m²)"
+              value={unitData[index]?.areaConstruir}
+              onChange={(e) =>
+                handleChange(index, "areaConstruir", e.target.value)
+              }
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Área Existente(m²)"
+              value={unitData[index]?.areaExistente}
+              onChange={(e) =>
+                handleChange(index, "areaExistente", e.target.value)
+              }
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Área de Uso Exclusivo(m²)"
+              value={unitData[index]?.areaExclusiva}
+              onChange={(e) =>
+                handleChange(index, "areaExclusiva", e.target.value)
+              }
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Área Comum Proporcional(m²)"
+              value={unitData[index]?.areaComum}
+              onChange={(e) => handleChange(index, "areaComum", e.target.value)}
+            />
+          </div>
           {unitData[index]?.processType && (
             <div>
               <button onClick={() => handleAddPavement(index)}>
@@ -195,71 +168,6 @@ const AddUnitButton: React.FC = () => {
               </button>
             </div>
           )}
-          {unitData[index]?.pavements.map((pavement, pavementIndex) => (
-            <div key={pavementIndex}>
-              <div>Pavimento {pavementIndex + 1}</div>
-              <div>
-                <label>Área a Construir(m²):</label>
-                <input
-                  type="text"
-                  value={pavement.areaConstruir}
-                  onChange={(e) =>
-                    handleChangePavement(
-                      index,
-                      pavementIndex,
-                      "areaConstruir",
-                      e.target.value
-                    )
-                  }
-                />
-              </div>
-              <div>
-                <label>Área Existente(m²):</label>
-                <input
-                  type="text"
-                  value={pavement.areaExistente}
-                  onChange={(e) =>
-                    handleChangePavement(
-                      index,
-                      pavementIndex,
-                      "areaExistente",
-                      e.target.value
-                    )
-                  }
-                />
-              </div>
-              <div>
-                <label>Área de Uso Exclusivo(m²):</label>
-                <input
-                  type="text"
-                  value={pavement.areaExclusiva}
-                  onChange={(e) =>
-                    handleChangePavement(
-                      index,
-                      pavementIndex,
-                      "areaExclusiva",
-                      e.target.value
-                    )
-                  }
-                />
-              </div>
-              <div>
-                <label>Área Comum Proporcional(m²):</label>
-                <input
-                  type="text"
-                  value={pavement.areaComum}
-                  onChange={(e) =>
-                    handleChangePavement(
-                      index,
-                      pavementIndex,
-                      "areaComum",
-                      e.target.value
-                    )
-                  }
-                />
-              </div>
-            </div>
-          ))}
         </div>
       ))}
     </div>
