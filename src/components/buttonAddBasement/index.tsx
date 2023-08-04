8bd1f2ffa2f676dac1956f104f828d786810e675
pablo <wademcfild@gmail.com>
2023-08-04 10:08:34 -0300

    correção de método handle

--- src/components/buttonAddBasement/index.tsx
+++ src/components/buttonAddBasement/index.tsx
@@ -3,11 +3,15 @@ import React, { useState } from 'react';
 const AdicionarPavimentoButton = () => {
   const [pavimentos, setPavimentos] = useState<string[]>(['1']);
 
-  const handleAdicionarPavimento = (event) => {
-    if (event.key === 'Enter') {
-      const proximoNumeroPavimento = pavimentos.length + 1;
-      setPavimentos([...pavimentos, proximoNumeroPavimento.toString()]);
-    }
+  const handleAdicionarPavimento = () => {
+    const proximoNumeroPavimento = pavimentos.length + 1;
+    setPavimentos([...pavimentos, proximoNumeroPavimento.toString()]);
+  };
+
+  const handleRemoverPavimento = (index) => {
+    const novosPavimentos = [...pavimentos];
+    novosPavimentos.splice(index, 1);
+    setPavimentos(novosPavimentos);
   };
 
   return (
@@ -17,15 +21,16 @@ const AdicionarPavimentoButton = () => {
           <input
             type="text"
             placeholder={`${pavimento}º Pavimento`}
-            onKeyDown={handleAdicionarPavimento}
             onChange={(event) => {
               const novosPavimentos = [...pavimentos];
               novosPavimentos[index] = event.target.value;
               setPavimentos(novosPavimentos);
             }}
           />
+          <button onClick={() => handleRemoverPavimento(index)}>-</button>
         </div>
       ))}
+      <button onClick={handleAdicionarPavimento}>+</button>
     </div>
   );
 };
