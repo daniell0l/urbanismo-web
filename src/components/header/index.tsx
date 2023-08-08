0f621b853cf167d9fec3accb82348edb062f81e8
Sr.Carrilho <126430073+WadeMcfild@users.noreply.github.com>
2023-08-08 15:31:50 -0300

    estilização do header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import logoWide from '../../assets/imgs/logoWide.png'
 import './style.css'
 
 interface HeaderProps {
   title: string;
 }
 
-const Header: React.FC<HeaderProps> = ({ title }) => {
+const Header: React.FC<HeaderProps> = ({}) => {
   return (
     <header>
       <div className='headerUrbanismo'>
+        <img src={logoWide} alt="logoWide" />
       </div>
     </header>
   );
