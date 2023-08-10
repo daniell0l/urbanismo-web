30177bc89106d4b300d9aa6f5deccd921c423dfb
pablo <wademcfild@gmail.com>
2023-08-10 11:54:38 -0300

    estilização do header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
-import logoWide from '../../assets/imgs/logoWide.png'
-import './style.css'
+import React, { useState } from 'react';
+import logoWide from "../../assets/imgs/logoWide.png";
+import "./style.css";
+import { FaUser } from "react-icons/fa";
 
 interface HeaderProps {
   title: string;
 }
 
-const Header: React.FC<HeaderProps> = ({}) => {
+const Header: React.FC<HeaderProps> = ({ title }) => {
+  const [showOptions, setShowOptions] = useState(false);
+
+  const toggleOptions = () => {
+    setShowOptions(!showOptions);
+  };
+
   return (
     <header>
-      <div className='headerUrbanismo'>
+      <div className="headerUrbanismo">
         <img src={logoWide} alt="logoWide" />
+        <div className="userIcon">
+          <div className="iconWrapper" onClick={toggleOptions}>
+            <FaUser />
+          </div>
+          {showOptions && (
+            <select className="userSelector">
+              <option value="user1">Usuário 1</option>
+              <option value="user2">Usuário 2</option>
+            </select>
+          )}
+        </div>
       </div>
     </header>
   );
