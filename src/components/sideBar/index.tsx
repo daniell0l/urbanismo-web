64ab29a49b910e0785c6348ecec66ee5fc06a5b2
pablo <wademcfild@gmail.com>
2023-08-14 11:20:26 -0300

    estilização geral da side-bar

--- src/components/sideBar/index.tsx
+++ src/components/sideBar/index.tsx
@@ -4,7 +4,7 @@ import "./style.css";
 interface SideBarProps {
 }
 
-const SideBar: React.FC<SideBarProps> = (props) => {
+const SideBar: React.FC<SideBarProps> = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [showProcessOptions, setShowProcessOptions] = useState(false);
 
@@ -24,7 +24,21 @@ const SideBar: React.FC<SideBarProps> = (props) => {
       </div>
       <div className="menu" onClick={(e) => e.stopPropagation()}>
         <div className="menu-item" onClick={toggleProcessOptions}>
-          Cadastrar Processos
+          Cadastrar processos
+          {showProcessOptions ? <div className="arrow">▼</div> : <div className="arrow">▶</div>}
+        </div>
+        {showProcessOptions && (
+          <div className="submenu">
+            <div className="submenu-item">Alvará Construção</div>
+            <div className="submenu-item">Alvará Regularização</div>
+            <div className="submenu-item">Condomínio</div>
+            <div className="submenu-item">Redimensionamento</div>
+          </div>
+        )}
+      </div>
+      <div className="menu" onClick={(e) => e.stopPropagation()}>
+        <div className="menu-item" onClick={toggleProcessOptions}>
+          Listar processos
           {showProcessOptions ? <div className="arrow">▼</div> : <div className="arrow">▶</div>}
         </div>
         {showProcessOptions && (
