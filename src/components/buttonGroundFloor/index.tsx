34dc6ff90fe39ad6a1811effa825a96732533832
Sr.Carrilho <126430073+WadeMcfild@users.noreply.github.com>
2023-08-17 14:27:15 -0300

    criação do botão de adicionar térreo

---
+++ src/components/buttonGroundFloor/index.tsx
@@ -0,0 +1,45 @@
+import { useState, ChangeEvent } from 'react';
+
+const AddGroundFloorButton = () => {
+  const [pavimentos, setPavimentos] = useState<string[]>(['']);
+
+  const handleAdicionarPavimento = () => {
+    setPavimentos([...pavimentos, '']);
+  };
+
+  const handleRemoverPavimento = (index: number) => {
+    if (pavimentos.length === 1) {
+      return;
+    }
+    const novosPavimentos = [...pavimentos];
+    novosPavimentos.splice(index, 1);
+    setPavimentos(novosPavimentos);
+  };
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>, index: number) => {
+    const novosPavimentos = [...pavimentos];
+    novosPavimentos[index] = event.target.value;
+    setPavimentos(novosPavimentos);
+  };
+
+  return (
+    <div>
+      {pavimentos.map((pavimento, index) => (
+        <div key={index}>
+          <input
+            type="text"
+            placeholder={`${index + 1}º Térreo`}
+            value={pavimento}
+            onChange={(event) => handleInputChange(event, index)}
+          />
+        </div>
+      ))}
+      <button type="button" onClick={handleAdicionarPavimento}>+</button>
+      {pavimentos.length > 1 && (
+        <button onClick={() => handleRemoverPavimento(pavimentos.length - 1)}>-</button>
+      )}
+    </div>
+  );
+};
+
+export default AddGroundFloorButton;
