b21488ce0d547db76fc2c851fdd823d42c564a69
Sr.Carrilho <126430073+WadeMcfild@users.noreply.github.com>
2023-08-17 18:46:27 -0300

    correções de estilo

--- src/components/buttonAddBasement/index.tsx
+++ src/components/buttonAddBasement/index.tsx
@@ -37,7 +37,7 @@ const AdicionarPavimentoButton = () => {
       ))}
       <button type="button" onClick={handleAdicionarPavimento}>+</button>
       {pavimentos.length > 1 && (
-        <button type="button" onClick={() => handleRemoverPavimento(pavimentos.length - 1)}>-</button>
+        <button onClick={() => handleRemoverPavimento(pavimentos.length - 1)}>-</button>
       )}
     </div>
   );
