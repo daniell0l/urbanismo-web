dc472e5b299154e8bb98b2e5a12c493a5677fbad
pablo <wademcfild@gmail.com>
2023-08-22 14:51:57 -0300

    todo o css do header foi trocado por tailwind

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { FaUser } from 'react-icons/fa';
 import logoWide from '../../assets/imgs/logoWide.png';
-import './style.css';
 
 interface HeaderProps {
   title: string;
@@ -20,30 +19,31 @@ const Header: React.FC<HeaderProps> = () => {
   };
 
   return (
-    <header>
-      <div className="headerUrbanismo">
-        <NavLink to="/principal">
-          <img src={logoWide} alt="logoWide" />
-        </NavLink>
-        <div
-          className="userIcon"
-          onMouseEnter={showOptionsHandler}
-          onMouseLeave={hideOptionsHandler}
-        >
-          <FaUser className="customUserIcon" />
-          {showOptions && (
-            <div
-              className="userSelector"
-              onMouseEnter={showOptionsHandler}
-              onMouseLeave={hideOptionsHandler}
-            >
-              <div className="relative">
-                <a href="/profile">Perfil</a>
-                <a href="#">Sair</a>
-              </div>
-            </div>
-          )}
+    <header className="h-16 fixed top-0 left-0 right-0 bg-blue-600 flex items-center justify-between px-6 z-10">
+      <NavLink to="/principal">
+        <img className='relative w-40 h-40 object-contain invert brightness-0 right-5' src={logoWide} alt="logoWide"
+        />
+      </NavLink>
+      <div
+        className="relative"
+        onMouseEnter={showOptionsHandler}
+        onMouseLeave={hideOptionsHandler}
+      >
+        <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center shadow-md ">
+          <FaUser className="text-lg text-blue-600 cursor-pointer w-10 h-7 rounded-b-full " />
         </div>
+        {showOptions && (
+          <div
+            className="absolute right-0 mt-0 bg-white border border-gray-300 shadow-md p-2 rounded-md"
+            onMouseEnter={showOptionsHandler}
+            onMouseLeave={hideOptionsHandler}
+          >
+            <div>
+              <a className='text-blue-600 font-bold flex hover:text-blue-800'href="/profile">Perfil</a>
+              <a className='text-blue-600 font-bold flex hover:text-blue-800'href="#">Sair</a>
+            </div>
+          </div>
+        )}
       </div>
     </header>
   );
